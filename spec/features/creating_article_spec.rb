590f8e8c219f42ab8fd929b455f5a03363ca2874
abdelmaseeh <abdelmaseeh.hanna@gmail.com>
2018-07-30 16:20:37 +0200

    Complete article validations and flash

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -13,4 +13,19 @@ RSpec.feature "Ceating article" do
     expect(page).to have_content("Article has been created")
     expect(page.current_path).to eq(articles_path)
   end
+
+  scenario "A user fail to create article" do
+    visit "/"
+    click_link "New Article"
+
+    fill_in "Title", with: "Creating a blog"
+    fill_in "Body", with: "This is a body"
+
+    click_button "Create Article"
+
+    expect(page).to have_content("Article has not been created")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
+
+  end
 end
